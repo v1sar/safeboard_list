6aa4d1ad0dd251a269b0e0ef84496ddc0f24d8a6
movchan <dmitry.movchan@kaspersky.com>
2018-12-20 12:05:54 +0300

    finish

--- app/src/main/java/ru/androidacademy/msk/lists/data/Actor.java
+++ app/src/main/java/ru/androidacademy/msk/lists/data/Actor.java
@@ -2,34 +2,34 @@ package ru.androidacademy.msk.lists.data;
 
 public class Actor {
 
-  private final String name;
-  private final String avatarUrl;
-  private final boolean hasOscar;
+    private final String name;
+    private final String avatarUrl;
+    private final boolean hasOscar;
 
-  public Actor(String name, String avatarUrl, boolean hasOscar) {
-    this.name = name;
-    this.avatarUrl = avatarUrl;
-    this.hasOscar = hasOscar;
-  }
+    public Actor(String name, String avatarUrl, boolean hasOscar) {
+        this.name = name;
+        this.avatarUrl = avatarUrl;
+        this.hasOscar = hasOscar;
+    }
 
-  public String getName() {
-    return name;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public String getAvatarUrl() {
-    return avatarUrl;
-  }
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
 
-  public boolean hasOscar() {
-    return hasOscar;
-  }
+    public boolean hasOscar() {
+        return hasOscar;
+    }
 
-  @Override
-  public String toString() {
-    return "Actor{" +
-        "name='" + name + '\'' +
-        ", avatarUrl=" + avatarUrl +
-        ", hasOscar=" + hasOscar +
-        '}';
-  }
+    @Override
+    public String toString() {
+        return "Actor{" +
+                "name='" + name + '\'' +
+                ", avatarUrl=" + avatarUrl +
+                ", hasOscar=" + hasOscar +
+                '}';
+    }
 }
